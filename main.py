05fff12baa6e3dcf30efa5d4105d34c0beff1c8e
teesloane <tylersloane@gmail.com>
2018-10-05 15:30:40 -0400

    Save sketching for another day.

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
-""" Sing that song. """
+""" Sing that song. 
+- TODO - Make runnable from anywhere 
+"""
 import os
 import shutil
 from glob import glob
-import argparse
-import scipy.io.wavfile
-import numpy as np
 from pydub import AudioSegment
 from halo import Halo
 
-
 SONGS = []
 
-TMP_FOL    = "./tmp/"       # DO NOT TOUCH, gets RECURSIVELY DELETED
-MIX_FOL    = "../mix/"      # FIXME will eventually be "where the cmd is run."  Currently hardcoded.
-EXPORT_FOL = "./export/"    # DO NOT TOUCH, can get RECURSIVELY DELETED
-TRACKLIST  = EXPORT_FOL + "./tracklist.txt"
+TMP_FOL = "./tmp/"  # DO NOT TOUCH, gets RECURSIVELY DELETED
+MIX_FOL = "../mix/"  # FIXME should be "where the cmd is run."  Currently hardcoded.
+EXPORT_FOL = "./export/"  # DO NOT TOUCH, can get RECURSIVELY DELETED
+TRACKLIST = EXPORT_FOL + "./tracklist.txt"
 
 
 @Halo(text="Loading songs as AudioSegments", spinner="arrow3")
 def load_songs(song_list, ext):
     """Load songs from mix folder and convert to AudioSegments + data"""
-    for file in glob(MIX_FOL + "*" + ext)[:2]:
+    for file in glob(MIX_FOL + "*" + ext)[:2]:  # DONT YOU, FORGET ABOUT FIXME
         audio_data = {
             "name": file.replace("../mix/", "").replace(ext, ""),
             "path:": file,
@@ -63,8 +61,8 @@ def setup():
 def mp3_list_to_wav(lst):
     """Convert list of mp3s to wavs. Adds wav path to the SONGS list."""
     for file in lst:
-        wav_path = TMP_FOL + file['name'] + ".wav"  # get proper wave file name.
-        with open(wav_path, "wb") as ftwo:          # write wavs to temp folder.
+        wav_path = TMP_FOL + file["name"] + ".wav"  # get proper wave file name.
+        with open(wav_path, "wb") as ftwo:  # write wavs to temp folder.
             file["mp3"].export(ftwo, format="wav")
             file["wav_path"] = wav_path
 
@@ -97,43 +95,19 @@ def export_playlist(playlist):
     playlist.export(out_f, format="mp3")
 
 
-def analysis():
-    """Basic analysis; mutate audio objects to store results
-    # TODO properly output a CSV for each song."""
-
-    print("Running Analysis...")
-    mp3_list_to_wav(SONGS)
-    for audio in SONGS:
-        rate, aud_data = scipy.io.wavfile.read(audio["wav_path"])
-        audio["rate"] = rate
-        audio["aud_data"] = aud_data
-        # 1000 == "step" values for the array.
-        audio["amp_x_time"] = np.arange(0, float(aud_data.shape[0]), 1000) / rate
-
-    # ch1 = aud_data[:, 0]
-    # ch2 = aud_data[:, 1]
-    np.savetxt("testfile", SONGS[1]["amp_x_time"], newline="\n")  # makes a giant file.
-    # print("song length is", SONGS[0]['name'], (aud_data.shape[0] / rate) / 60)
-
-
 def teardown():
     """Remove temporary files"""
     shutil.rmtree(TMP_FOL)  # This is probaby a bad idea.
     print("Playlist exported to: './export/ folder'")
 
 
-def main(args):
+def main():
     """sing some songs"""
     setup()
     load_songs(SONGS, ".mp3")
     mixdown()
-    if args.a:
-        analysis()
     teardown()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-a", help="Run audio analysis on file.", action="store_true")
-    args = parser.parse_args()
-    main(args)
+    main()
--- sketch.py
+++
@@ -1,7 +0,0 @@
-def setup():
-    size(400, 400)
-
-def draw():
-    background(255)
-    fill(0)
-    ellipse(mouseX, mouseY, 50, 50)
