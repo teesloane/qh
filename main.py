ef286c310bb55de265628c7e25d00520f1c85617
teesloane <tylersloane@gmail.com>
2018-10-05 10:38:50 -0400

    spinners, folder checking, parse args
    - add spinners (Halo package) for long running tasks
    - add argparse for checking if user wants to run analysis on files
    - remove setup_paths function and return to using relative paths

--- main.py
+++ main.py
@@ -2,27 +2,25 @@
 import os
 import shutil
 from glob import glob
+import argparse
 import scipy.io.wavfile
 import numpy as np
 from pydub import AudioSegment
+from halo import Halo
+
 
 SONGS = []
-RUN_ANALYSIS: False
-QH_FOL = MIX_FOL = TMP_FOL = EXPORT_FOL = TRACKLIST = None
+
+TMP_FOL    = "./tmp/"       # DO NOT TOUCH, gets RECURSIVELY DELETED
+MIX_FOL    = "../mix/"      # FIXME will eventually be "where the cmd is run."  Currently hardcoded.
+EXPORT_FOL = "./export/"    # DO NOT TOUCH, can get RECURSIVELY DELETED
+TRACKLIST  = EXPORT_FOL + "./tracklist.txt"
 
 
-def setup_paths():
-    global TMP_FOL, MIX_FOL, EXPORT_FOL, TRACKLIST  # bad bad bad -> switch to a class
-    TMP_FOL = QH_FOL + "/program/tmp/"
-    MIX_FOL = QH_FOL + "/mix/"
-    EXPORT_FOL = QH_FOL + "/export/"
-    TRACKLIST = EXPORT_FOL + "./tracklist.txt"
-
-
+@Halo(text="Loading songs as AudioSegments", spinner="arrow3")
 def load_songs(song_list, ext):
     """Load songs from mix folder and convert to AudioSegments + data"""
-    print("Loading songs as AudioSegments...")
-    for file in glob(MIX_FOL + "*" + ext):
+    for file in glob(MIX_FOL + "*" + ext)[:2]:
         audio_data = {
             "name": file.replace("../mix/", "").replace(ext, ""),
             "path:": file,
@@ -36,9 +34,23 @@ def load_songs(song_list, ext):
 
 
 def setup():
-    """set up temp folder, convert to wav"""
+    """Check for/remove/setup necessary folders"""
 
-    setup_paths()
+    if os.path.exists(EXPORT_FOL):
+        choice = input("Export folder already exists, proceed and overwrite? [Y/n]: ")
+        if choice == "Y":
+            shutil.rmtree(EXPORT_FOL)
+        else:
+            print("Exiting!")
+            exit()
+
+    if not os.path.exists(MIX_FOL):
+        print("No 'mix' folder found.")
+        exit()
+
+    if glob(MIX_FOL + "*.mp3") == []:
+        print("No mp3 files found in 'mix' folder")
+        exit()
 
     if not os.path.exists(TMP_FOL):
         os.makedirs(TMP_FOL)
@@ -46,43 +58,50 @@ def setup():
     if not os.path.exists(EXPORT_FOL):
         os.makedirs(EXPORT_FOL)
 
-    load_songs(SONGS, ".mp3")
 
-
+@Halo(text="Converting MP3s to WAV for analysis", spinner="bouncingBall")
 def mp3_list_to_wav(lst):
     """Convert list of mp3s to wavs. Adds wav path to the SONGS list."""
     for file in lst:
-        ## TODO move this into setup / make it a key on the objects.
-        ## TODO: revert to doing this with relative paths based on where the script is executed.
-        file_name = file["name"].split("/")[-1]  
-        wav_path = TMP_FOL + file_name + ".wav"
-        print("file name is", file_name)
-        with open(wav_path, "wb") as ftwo:
+        wav_path = TMP_FOL + file['name'] + ".wav"  # get proper wave file name.
+        with open(wav_path, "wb") as ftwo:          # write wavs to temp folder.
             file["mp3"].export(ftwo, format="wav")
             file["wav_path"] = wav_path
 
 
 def mixdown():
-    songs_sorted = sorted(SONGS, key=lambda k: k["name"])
+    """Creates a playlist and saves to file."""
+    # Spinner can't be a decorator or it glitches with export_playlist
+    spinner = Halo(text="Building playlist...️", spinner="earth")
+    spinner.start()
+
+    s_sorted = sorted(SONGS, key=lambda k: k["name"])
     tracklist = open(TRACKLIST, "a")
-    tracklist.write(songs_sorted[0]["name"] + "\n")
-    playlist = songs_sorted[0]["mp3"]
-    for song in songs_sorted[1:]:
-        print("Appending:", song["name"])
+    playlist = AudioSegment.empty()
+
+    # Write playlist and tracklist to memory.
+    for song in s_sorted:  # Concat audio objects into full playlist.
         audio = song["mp3"]
         tracklist.write(song["name"] + "\n")
-        # playlist.append(audio, crossfade=2000) # this fails, sadly, so no crossfade .... :
         playlist = playlist + audio
 
     tracklist.close()
+    spinner.stop()
+    export_playlist(playlist)
 
+
+@Halo(text="Saving playlist...", spinner="moon")
+def export_playlist(playlist):
+    """Saves playlist to file"""
     out_f = open(EXPORT_FOL + "/output.mp3", "wb")
     playlist.export(out_f, format="mp3")
 
 
 def analysis():
     """Basic analysis; mutate audio objects to store results
-    TODO: properly output a CSV for each song."""
+    # TODO properly output a CSV for each song."""
+
+    print("Running Analysis...")
     mp3_list_to_wav(SONGS)
     for audio in SONGS:
         rate, aud_data = scipy.io.wavfile.read(audio["wav_path"])
@@ -99,23 +118,22 @@ def analysis():
 
 def teardown():
     """Remove temporary files"""
-    shutil.rmtree(TMP_FOL)  # It bad!
+    shutil.rmtree(TMP_FOL)  # This is probaby a bad idea.
+    print("Playlist exported to: './export/ folder'")
 
 
-def main():
+def main(args):
     """sing some songs"""
     setup()
+    load_songs(SONGS, ".mp3")
     mixdown()
-    if RUN_ANALYSIS:
+    if args.a:
         analysis()
     teardown()
 
 
 if __name__ == "__main__":
-    QH_FOL = input("Path to playlist folder: ")
-    run_analysis = input("Run analysis? [Y/n]: ")
-
-    if run_analysis == "Y":
-        RUN_ANALYSIS = True
-
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", help="Run audio analysis on file.", action="store_true")
+    args = parser.parse_args()
+    main(args)
